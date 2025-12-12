ca142b9bd45f07e401dc1841dad5dcee9fd545cc
Rumman Ahmed <ahmed.rumman@gmail.com>
2025-12-12 12:32:34 +1100

    Fix Next.js Turbopack build issues by removing webpack config conflicts

--- next.config.ts
+++ next.config.ts
@@ -9,32 +9,32 @@ const nextConfig: NextConfig = {
   },
 
   // Suppress harmless SES lockdown warnings in development
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.devtool = 'cheap-module-source-map';
-    }
+  // webpack: (config, { dev, isServer }) => {
+  //   if (dev && !isServer) {
+  //     config.devtool = 'cheap-module-source-map';
+  //   }
 
-    // Performance optimizations for development
-    if (dev) {
-      // Reduce bundle size in development
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          ...config.optimization.splitChunks,
-          cacheGroups: {
-            ...config.optimization.splitChunks?.cacheGroups,
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name: 'vendors',
-              chunks: 'all',
-            },
-          },
-        },
-      };
-    }
+  //   // Performance optimizations for development
+  //   if (dev) {
+  //     // Reduce bundle size in development
+  //     config.optimization = {
+  //       ...config.optimization,
+  //       splitChunks: {
+  //         ...config.optimization.splitChunks,
+  //         cacheGroups: {
+  //           ...config.optimization.splitChunks?.cacheGroups,
+  //           vendor: {
+  //             test: /[\\/]node_modules[\\/]/,
+  //             name: 'vendors',
+  //             chunks: 'all',
+  //           },
+  //         },
+  //       };
+  //     };
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 export default nextConfig;
